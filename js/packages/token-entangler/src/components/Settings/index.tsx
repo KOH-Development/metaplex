7fea9945e38fbd01583bb412a0593b2d0cb1a6d5
Rovendoug <rovendoug@gmail.com>
2021-12-22 14:38:58 -0600

    Reverting back to last known working build for gumdrop and adding claim.tsx changes.

--- js/packages/token-entangler/src/components/Settings/index.tsx
+++ js/packages/token-entangler/src/components/Settings/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { useWallet } from '@solana/wallet-adapter-react';
-import { ENDPOINTS, useColorMode, useConnectionConfig } from '../../contexts';
-import { CopyOutlined } from '@ant-design/icons';
-import { ModalEnum, useModal, useWalletModal } from '../../contexts';
+import React from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { ENDPOINTS, useColorMode, useConnectionConfig } from "../../contexts";
+import { notify, shortenAddress } from "@oyster/common";
+import { CopyOutlined } from "@ant-design/icons";
+import { ModalEnum, useModal, useWalletModal } from "../../contexts";
 import {
   Box,
   Button,
@@ -18,54 +19,16 @@ import {
   MenuItem,
   Select,
   Stack,
-} from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import { notification } from 'antd';
 
-// shorten the checksummed version of the input address to have 4 characters at start and end
-export function shortenAddress(address: string, chars = 4): string {
-  return `${address.slice(0, chars)}...${address.slice(-chars)}`;
-}
-
-// import Link from '../components/Link';
-
-export function notify({
-  message = '',
-  description = undefined as any,
-  txid = '',
-  type = 'info',
-  placement = 'bottomLeft',
-}) {
-  if (txid) {
-    //   <Link
-    //     external
-    //     to={'https://explorer.solana.com/tx/' + txid}
-    //     style={{ color: '#0000ff' }}
-    //   >
-    //     View transaction {txid.slice(0, 8)}...{txid.slice(txid.length - 8)}
-    //   </Link>
-
-    description = <></>;
-  }
-  (notification as any)[type]({
-    message: <span style={{ color: 'black' }}>{message}</span>,
-    description: (
-      <span style={{ color: 'black', opacity: 0.5 }}>{description}</span>
-    ),
-    placement,
-    style: {
-      backgroundColor: 'white',
-    },
-  });
-}
-
-export const Settings = ({ narrow }: { narrow: boolean }) => {
+export const Settings = ({ narrow } : { narrow : boolean }) => {
   const { disconnect, publicKey } = useWallet();
   const { setEndpoint, env, endpoint } = useConnectionConfig();
   const { setVisible } = useWalletModal();
@@ -85,13 +48,14 @@ export const Settings = ({ narrow }: { narrow: boolean }) => {
         if (publicKey) {
           await navigator.clipboard.writeText(publicKey.toBase58());
           notify({
-            message: 'Wallet update',
-            description: 'Address copied to clipboard',
+            message: "Wallet update",
+            description: "Address copied to clipboard",
           });
         }
       },
-      innerNarrow: () =>
-        `Copy Address (${publicKey && shortenAddress(publicKey.toBase58())})`,
+      innerNarrow: () => (
+        `Copy Address (${publicKey && shortenAddress(publicKey.toBase58())})`
+      ),
       inner: function ConnectedWalletCopyC() {
         return (
           <React.Fragment>
@@ -103,28 +67,24 @@ export const Settings = ({ narrow }: { narrow: boolean }) => {
     },
     {
       click: open,
-      inner: () => 'Change\u00A0Wallet',
+      inner: () => "Change\u00A0Wallet",
     },
     {
       click: () => disconnect().catch(),
       inner: () => `Disconnect\u00A0(${env})`,
-      expandedExtra: {
-        // these are interepreted as props. TODO: specific types
-        color: 'error' as any,
-        variant: 'contained' as any,
-      },
+      expandedExtra: { // these are interepreted as props. TODO: specific types
+        color: "error" as any,
+        variant: "contained" as any,
+      }
     },
   ];
 
   const [drawerOpen, setDrawerOpen] = React.useState(false);
   const [envCollapseOpen, setEnvCollapseOpen] = React.useState(false);
 
-  const hackySkipSet = 'hackySkipSet';
-  const toggleDrawer = open => event => {
-    if (
-      event.type === 'keydown' &&
-      (event.key === 'Tab' || event.key === 'Shift')
-    ) {
+  const hackySkipSet = "hackySkipSet";
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
     }
 
@@ -135,13 +95,17 @@ export const Settings = ({ narrow }: { narrow: boolean }) => {
     setDrawerOpen(open);
   };
 
-  const drawerC = inner => {
+  const drawerC = (inner) => {
     return (
       <React.Fragment>
         <Button onClick={toggleDrawer(true)}>
           <AccountBalanceWalletIcon />
         </Button>
-        <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <Drawer
+          anchor="right"
+          open={drawerOpen}
+          onClose={toggleDrawer(false)}
+        >
           <Box
             sx={{ width: 250 }}
             role="presentation"
@@ -161,7 +125,7 @@ export const Settings = ({ narrow }: { narrow: boolean }) => {
       onClick={colorModeCtx.toggleColorMode}
       color="inherit"
     >
-      {theme.palette.mode === 'dark' ? (
+      {theme.palette.mode === "dark" ? (
         <Brightness7Icon />
       ) : (
         <Brightness4Icon />
@@ -175,7 +139,7 @@ export const Settings = ({ narrow }: { narrow: boolean }) => {
         <ListItemText
           primary="Wallet"
           primaryTypographyProps={{
-            fontSize: '1.2rem',
+            fontSize: "1.2rem",
             fontWeight: 'medium',
             letterSpacing: 0,
           }}
@@ -184,50 +148,50 @@ export const Settings = ({ narrow }: { narrow: boolean }) => {
     );
     return (
       <React.Fragment>
-        {!publicKey &&
-          drawerC(
-            <List>
-              {listHead}
-              <Divider />
-              <ListItemButton
-                onClick={() => setEnvCollapseOpen(!envCollapseOpen)}
-                className={hackySkipSet}
-              >
-                Change Network
-                {envCollapseOpen ? <ExpandLess /> : <ExpandMore />}
-              </ListItemButton>
-              <Collapse in={envCollapseOpen} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                  {ENDPOINTS.map(p => (
-                    <ListItemButton
-                      selected={endpoint === p.endpoint}
-                      onClick={() => setEndpoint(p.endpoint)}
-                      key={p.name}
-                      sx={{ pl: 4 }}
-                      className={hackySkipSet}
-                    >
-                      {p.name}
-                    </ListItemButton>
-                  ))}
-                </List>
-              </Collapse>
-              <ListItemButton onClick={handleConnect}>Connect</ListItemButton>
-            </List>,
-          )}
-        {publicKey &&
-          drawerC(
-            <List>
-              {listHead}
-              <Divider />
-              {connectedActions.map((a, idx) => {
-                return (
-                  <ListItemButton onClick={a.click} key={idx}>
-                    {(a.innerNarrow && a.innerNarrow()) || a.inner()}
+        {!publicKey && drawerC(
+          <List>
+            {listHead}
+            <Divider />
+            <ListItemButton
+              onClick={() => setEnvCollapseOpen(!envCollapseOpen)}
+              className={hackySkipSet}
+            >
+              Change Network
+              {envCollapseOpen ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={envCollapseOpen} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                {ENDPOINTS.map(p => (
+                  <ListItemButton
+                    selected={endpoint === p.endpoint}
+                    onClick={() => setEndpoint(p.endpoint)}
+                    key={p.name}
+                    sx={{ pl: 4 }}
+                    className={hackySkipSet}
+                  >
+                    {p.name}
                   </ListItemButton>
-                );
-              })}
-            </List>,
-          )}
+                ))}
+              </List>
+            </Collapse>
+            <ListItemButton onClick={handleConnect}>
+              Connect
+            </ListItemButton>
+          </List>
+        )}
+        {publicKey && drawerC(
+          <List>
+            {listHead}
+            <Divider />
+            {connectedActions.map((a, idx) => {
+              return (
+                <ListItemButton onClick={a.click} key={idx}>
+                  {(a.innerNarrow && a.innerNarrow()) || a.inner()}
+                </ListItemButton>
+              );
+            })}
+          </List>
+        )}
         {themeSwitch}
       </React.Fragment>
     );
@@ -237,38 +201,36 @@ export const Settings = ({ narrow }: { narrow: boolean }) => {
         direction="row"
         spacing={2}
         sx={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-          alignItems: 'center',
-          marginRight: '36px',
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          marginRight: "36px",
         }}
       >
         {!publicKey && (
           <React.Fragment>
-            <FormControl variant="standard" style={{ minWidth: '10ch' }}>
+            <FormControl variant="standard" style={{minWidth: "10ch"}}>
               <Select
                 id="connected-env-select"
-                onChange={e => {
-                  setEndpoint(e.target.value);
-                }}
+                onChange={(e) => { setEndpoint(e.target.value); }}
                 value={endpoint}
               >
                 {ENDPOINTS.map(({ name, endpoint }) => (
-                  <MenuItem key={name} value={endpoint}>
-                    {name}
-                  </MenuItem>
+                  <MenuItem key={name} value={endpoint}>{name}</MenuItem>
                 ))}
               </Select>
             </FormControl>
             <Link underline="none">
-              <Button variant="contained" onClick={handleConnect}>
+              <Button
+                variant="contained"
+                onClick={handleConnect}
+              >
                 Connect
               </Button>
             </Link>
           </React.Fragment>
         )}
-        {publicKey &&
-          connectedActions.map((a, idx) => {
+        {publicKey && connectedActions.map((a, idx) => {
             return (
               <Button
                 key={idx}
@@ -279,7 +241,8 @@ export const Settings = ({ narrow }: { narrow: boolean }) => {
                 {a.inner()}
               </Button>
             );
-          })}
+          })
+        }
         {themeSwitch}
       </Stack>
     );
--- js/packages/token-entangler/src/index.tsx
+++ js/packages/token-entangler/src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 import {
+  AccountsProvider,
   WalletProvider,
   ConnectionProvider,
   ColorModeContextProvider,
-} from './contexts';
+} from "./contexts";
 
-import 'antd/dist/antd.css';
-import '@fontsource/open-sans';
-import '@fontsource/roboto';
-import '@fontsource/sora';
+import "antd/dist/antd.css";
+import "@fontsource/open-sans";
+import "@fontsource/roboto";
+import "@fontsource/sora";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 // import "./index.css";
 
@@ -21,13 +22,15 @@ ReactDOM.render(
   <React.StrictMode>
     <ConnectionProvider>
       <WalletProvider>
-        <ColorModeContextProvider>
-          <App />
-        </ColorModeContextProvider>
+        <AccountsProvider>
+          <ColorModeContextProvider>
+            <App />
+          </ColorModeContextProvider>
+        </AccountsProvider>
       </WalletProvider>
     </ConnectionProvider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 reportWebVitals();
